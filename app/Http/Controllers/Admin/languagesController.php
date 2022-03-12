921688adce9ef1d02c46a5a1629d430f0541e0da
firasabualkomboz <firasa018@gmail.com>
2022-03-12 23:02:10 +0200

    Refactor Language Model

--- app/Http/Controllers/Admin/languagesController.php
+++ app/Http/Controllers/Admin/languagesController.php
@@ -10,92 +10,89 @@ use App\Http\Requests\LanguageRequest;
 class languagesController extends Controller
 {
 
-public function index(){
+    public function index()
+    {
 
-$languages = Language::select()->paginate(PAGINATION_COUNT);
-return view('admin.languages.index', compact('languages'));
+        $languages = Language::select()->paginate(PAGINATION_COUNT);
+        return view('admin.languages.index', compact('languages'));
 
-} //end index
-
-
-public function create(){
-
-return view ('admin.languages.create');
-
-} //end create
+    } //end index
 
 
-   public function store(LanguageRequest $request ){
+    public function create()
+    {
 
-    // بعد الفحص نحفطه في الداتا بيز
+        return view('admin.languages.create');
 
-// return $request->except(['_token']); // هذا برجع json
-
-    // dd($request->all());
-
-    try{
-
-    Language::create($request->except(['_token']));
-
-    return redirect()->route('admin.languages')->with(['success'=>'تم إضافة اللغة بنجاح']);
-
-    }catch(\Exception $ex){
-
-    return redirect()->route('admin.languages')->with(['error'=>'يوجد خطأ في إضافة اللغة ']);
-
-    }
+    } //end create
 
 
-}
+    public function store(LanguageRequest $request)
+    {
 
+        try {
 
-public function edit($id){
+            Language::create($request->except(['_token']));
 
-    $language = Language::select()->find($id);
-    if(!$language){
-        return redirect()->route('admin.dashboard')->with(['error'=>'اللغة مش موجودة']);
-    }
-    return view ('admin.languages.edit',compact('language'));
+            return redirect()->route('admin.languages')->with(['success' => 'تم إضافة اللغة بنجاح']);
 
-}
+        } catch (\Exception $ex) {
 
+            return redirect()->route('admin.languages')->with(['error' => 'يوجد خطأ في إضافة اللغة ']);
 
-public function update($id,LanguageRequest $request){
-
-    try{
-    $language = Language::find($id);
-    if(!$language){
-        return redirect()->route('admin.languages.edit',$id)->with(['error'=>'اللغة مش موجودة']);
-    }
-    if(!$request->has('active'))
-    $request ->request->add(['active'=>0]);
-    // اذا لو موجودة اعمل ابديت في الداتا بيز
-    $language->update($request->except('_token'));
-    return redirect()->route('admin.languages')->with(['seccess'=>'تم التحديث بنجاح']);
-    }catch
-    (\Exception $ex){
-        return redirect()->route('admin.languages')->with(['error'=>'باشا هناك خطأ ما بليزز حاول مرة تانية ']);
-    }
-}
-
-
-
-
-public function destroy($id)
-{
-    try{
-        $language = Language::find($id);
-        if(!$language){
-            return redirect()->route('admin.languages',$id)->with(['error'=>'اللغة مش موجودة']);
         }
 
-        $language->delete();
-        return redirect()->route('admin.languages')->with(['seccess'=>'تم الحذف بنجاح']);
-        }catch
-        (\Exception $ex){
-            return redirect()->route('admin.languages')->with(['error'=>'باشا هناك خطأ ما بليزز حاول مرة تانية ']);
+
+    }
+
+
+    public function edit($id)
+    {
+
+        $language = Language::select()->find($id);
+        if (!$language) {
+            return redirect()->route('admin.dashboard')->with(['error' => 'اللغة مش موجودة']);
         }
-}
+        return view('admin.languages.edit', compact('language'));
+
+    }
+
+
+    public function update($id, LanguageRequest $request)
+    {
+
+        try {
+            $language = Language::find($id);
+            if (!$language) {
+                return redirect()->route('admin.languages.edit', $id)->with(['error' => 'اللغة مش موجودة']);
+            }
+            if (!$request->has('active'))
+                $request->request->add(['active' => 0]);
+            // اذا لو موجودة اعمل ابديت في الداتا بيز
+            $language->update($request->except('_token'));
+            return redirect()->route('admin.languages')->with(['seccess' => 'تم التحديث بنجاح']);
+        } catch
+        (\Exception $ex) {
+            return redirect()->route('admin.languages')->with(['error' => 'باشا هناك خطأ ما بليزز حاول مرة تانية ']);
+        }
+    }
+
+
+    public function destroy($id)
+    {
+        try {
+            $language = Language::find($id);
+            if (!$language) {
+                return redirect()->route('admin.languages', $id)->with(['error' => 'اللغة مش موجودة']);
+            }
+
+            $language->delete();
+            return redirect()->route('admin.languages')->with(['seccess' => 'تم الحذف بنجاح']);
+        } catch
+        (\Exception $ex) {
+            return redirect()->route('admin.languages')->with(['error' => 'باشا هناك خطأ ما بليزز حاول مرة تانية ']);
+        }
+    }
 
 }
 
--- routes/admin.php
+++ routes/admin.php
@@ -1,108 +1,98 @@
 <?php
+
 use Illuminate\Support\Facades\Route;
 
 //define('PAGINATION_COUNT',10);
 
-Route::group(['namespace'=>'Admin' ,  'middleware' => 'auth:admin'], function () {
+Route::group(['namespace' => 'Admin', 'middleware' => 'auth:admin'], function () {
 
-    Route::get('/', 'DashboardController@index')  ->name('admin.dashboard');
+    Route::get('/', 'DashboardController@index')->name('admin.dashboard');
 
 
 // ************************************ languages ************************************
 
-Route::group(['prefix' => 'languages'], function () {
+    Route::group(['prefix' => 'languages'], function () {
 
-Route::get('/', 'languagesController@index')->name('admin.languages');
-Route::get('create', 'languagesController@create')->name('admin.languages.create');
+        Route::get('/', 'languagesController@index')->name('admin.languages');
+        Route::get('create', 'languagesController@create')->name('admin.languages.create');
 //حفظ اللعة واضافتها
-Route::post('store', 'languagesController@store')->name('admin.languages.store');
-Route::get('edit/{id}', 'languagesController@edit')->name('admin.languages.edit');
-Route::post('update/{id}', 'languagesController@update')->name('admin.languages.update');
-Route::get('delete/{id}', 'languagesController@destroy')->name('admin.languages.delete');
+        Route::post('store', 'languagesController@store')->name('admin.languages.store');
+        Route::get('edit/{id}', 'languagesController@edit')->name('admin.languages.edit');
+        Route::post('update/{id}', 'languagesController@update')->name('admin.languages.update');
+        Route::get('delete/{id}', 'languagesController@destroy')->name('admin.languages.delete');
 
-});
+    });
 // ************************************ languages ************************************
-
 
 
 // ************************************ main categories ************************************
 
-Route::group(['prefix' => 'main_categories'], function () {
+    Route::group(['prefix' => 'main_categories'], function () {
 
-Route::get('/', 'MainCategoriesController@index')->name('admin.maincategories');
-Route::get('create', 'MainCategoriesController@create')->name('admin.maincategories.create');
+        Route::get('/', 'MainCategoriesController@index')->name('admin.maincategories');
+        Route::get('create', 'MainCategoriesController@create')->name('admin.maincategories.create');
 //حفظ اللعة واضافتها
-Route::post('store', 'MainCategoriesController@store')->name('admin.maincategories.store');
-Route::get('edit/{id}', 'MainCategoriesController@edit')->name('admin.maincategories.edit');
-Route::post('update/{id}', 'MainCategoriesController@update')->name('admin.maincategories.update');
-Route::get('delete/{id}', 'MainCategoriesController@destroy')->name('admin.maincategories.delete');
-Route::get('changeStatus/{id}', 'MainCategoriesController@changeStatus')->name('admin.maincategories.status');
+        Route::post('store', 'MainCategoriesController@store')->name('admin.maincategories.store');
+        Route::get('edit/{id}', 'MainCategoriesController@edit')->name('admin.maincategories.edit');
+        Route::post('update/{id}', 'MainCategoriesController@update')->name('admin.maincategories.update');
+        Route::get('delete/{id}', 'MainCategoriesController@destroy')->name('admin.maincategories.delete');
+        Route::get('changeStatus/{id}', 'MainCategoriesController@changeStatus')->name('admin.maincategories.status');
 
-});
+    });
 
 // ************************************ end  main categories ************************************
 
 
 // ************************************  Sub_categories ************************************
-Route::group(['prefix' => 'Sub_categories'], function () {
+    Route::group(['prefix' => 'Sub_categories'], function () {
 
 
-    Route::get('/', 'SectionController@index')->name('admin.subcategories');
-    Route::get('create', 'SectionController@create')->name('admin.subcategories.create');
-    Route::post('store', 'SectionController@store')->name('admin.subcategories.store');
-    // Route::get('edit/{id}', 'SectionController@edit')->name('admin.subCategoriesController.edit');
-    // Route::post('update/{id}', 'SectionController@update')->name('admin.subCategoriesController.update');
-    // Route::get('delete/{id}', 'SectionController@destroy')->name('admin.subCategoriesController.delete');
-    // Route::get('changeStatus/{id}', 'SectionController@changeStatus')->name('admin.subCategoriesController.status');
-});
+        Route::get('/', 'SectionController@index')->name('admin.subcategories');
+        Route::get('create', 'SectionController@create')->name('admin.subcategories.create');
+        Route::post('store', 'SectionController@store')->name('admin.subcategories.store');
+        // Route::get('edit/{id}', 'SectionController@edit')->name('admin.subCategoriesController.edit');
+        // Route::post('update/{id}', 'SectionController@update')->name('admin.subCategoriesController.update');
+        // Route::get('delete/{id}', 'SectionController@destroy')->name('admin.subCategoriesController.delete');
+        // Route::get('changeStatus/{id}', 'SectionController@changeStatus')->name('admin.subCategoriesController.status');
+    });
 
 // ************************************ end  Sub_categories ************************************
 
 // ************************************ product  ************************************
-Route::group(['prefix' => 'product'], function () {
+    Route::group(['prefix' => 'product'], function () {
 
-    Route::get('/', 'ProductController@index')->name('admin.product');
-    Route::get('create', 'ProductController@create')->name('admin.product.create');
-    // // //حفظ اللعة واضافتها
-    Route::post('store', 'ProductController@store')->name('admin.product.store');
-    // Route::get('edit/{id}', 'ProductController@edit')->name('admin.product.edit');
-    // Route::post('update/{id}', 'ProductController@update')->name('admin.subCategoriesController.update');
-    Route::get('delete/{id}', 'ProductController@destroy')->name('admin.product.delete');
-    // Route::get('changeStatus/{id}', 'ProductController@changeStatus')->name('admin.subCategoriesController.status');
+        Route::get('/', 'ProductController@index')->name('admin.product');
+        Route::get('create', 'ProductController@create')->name('admin.product.create');
+        // // //حفظ اللعة واضافتها
+        Route::post('store', 'ProductController@store')->name('admin.product.store');
+        // Route::get('edit/{id}', 'ProductController@edit')->name('admin.product.edit');
+        // Route::post('update/{id}', 'ProductController@update')->name('admin.subCategoriesController.update');
+        Route::get('delete/{id}', 'ProductController@destroy')->name('admin.product.delete');
+        // Route::get('changeStatus/{id}', 'ProductController@changeStatus')->name('admin.subCategoriesController.status');
 
-    Route::get('/addToCart/{product}', 'ProductController@addToCart')->name('cart.add');
-    Route::get('/shopping-cart', 'ProductController@showCart')->name('cart.show');
-    Route::get('/checkout/{amount}', 'ProductController@checkout')->name('cart.checkout');
+        Route::get('/addToCart/{product}', 'ProductController@addToCart')->name('cart.add');
+        Route::get('/shopping-cart', 'ProductController@showCart')->name('cart.show');
+        Route::get('/checkout/{amount}', 'ProductController@checkout')->name('cart.checkout');
 
-    Route::post('/charge', 'ProductController@charge')->name('cart.charge');
+        Route::post('/charge', 'ProductController@charge')->name('cart.charge');
 
-});
+    });
 
 // ************************************ end  product  ************************************
 
-
-// ************************************ permission  ************************************
-
-Route::group(['prefix' => 'permission'], function () {
-
-    Route::get('/', 'PermissionController@index')->name('admin.permission');
-    Route::get('create', 'PermissionController@create')->name('admin.permission.create');
-});
-
-
 // ************************************ vendors ************************************
-Route::group(['prefix' => 'vendors'], function () {
+    Route::group(['prefix' => 'vendors'], function () {
 
-    Route::get('/', 'VendorsController@index')->name('admin.vendors');
-    Route::get('create', 'VendorsController@create')->name('admin.vendors.create');
-    //حفظ اللعة واضافتها
-    Route::post('store', 'VendorsController@store')->name('admin.vendors.store');
-    Route::get('edit/{id}', 'VendorsController@edit')->name('admin.vendors.edit');
-    Route::post('update/{id}', 'VendorsController@update')->name('admin.vendors.update');
-    Route::get('delete/{id}', 'VendorsController@destroy')->name('admin.vendors.delete');
-    Route::get('changeStatus/{id}', 'VendorsController@changeStatus')->name('admin.vendors.status');
+        Route::get('/', 'VendorsController@index')->name('admin.vendors');
+        Route::get('create', 'VendorsController@create')->name('admin.vendors.create');
+        //حفظ اللعة واضافتها
+        Route::post('store', 'VendorsController@store')->name('admin.vendors.store');
+        Route::get('edit/{id}', 'VendorsController@edit')->name('admin.vendors.edit');
+        Route::post('update/{id}', 'VendorsController@update')->name('admin.vendors.update');
+        Route::get('delete/{id}', 'VendorsController@destroy')->name('admin.vendors.delete');
+        Route::get('changeStatus/{id}', 'VendorsController@changeStatus')->name('admin.vendors.status');
 
-});
+    });
 
 // ************************************ end  vendors ************************************
 
@@ -115,14 +105,4 @@ Route::group(['namespace' => 'Admin', 'middleware' => 'guest:admin'], function (
     Route::get('login', 'LoginController@getLogin')->name('get.admin.login');
     Route::POST('login', 'LoginController@login')->name('admin.login');
 
-});
-
-// Route::get('tester', function () {
-//
-//     return 'feras';
-//
-// });
-
-Route::get('admin-test' , function(){
-    return true; 
 });
